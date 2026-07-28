229d0fef853d6e1f314657403e03eb7bdc504a4b
abhinandh-s <168844210+abhinandh-s@users.noreply.github.com>
2026-07-28 18:40:35 +0000

    chore: compile rust to wasm

---
+++ lib/yt_bot.d.ts
@@ -0,0 +1,8 @@
+// @generated file from wasmbuild -- do not edit
+// deno-lint-ignore-file
+// deno-fmt-ignore-file
+
+export function get_channel_id(
+  api_key: string,
+  raw_handle: string,
+): Promise<string | undefined>;
