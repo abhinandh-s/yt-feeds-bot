a34e9b4f148b4b75e4442c89ebfcd2c8f20ac628
abhinandh-s <168844210+abhinandh-s@users.noreply.github.com>
2026-08-10 05:18:10 +0000

    chore: compile rust to wasm

--- lib/yt_bot.d.ts
+++ lib/yt_bot.d.ts
@@ -2,7 +2,16 @@
 // deno-lint-ignore-file
 // deno-fmt-ignore-file
 
-export function get_channel_id(
+export class ChannelDetails {
+  private constructor();
+  free(): void;
+  [Symbol.dispose](): void;
+  readonly id: string;
+  readonly thumbnail_url: string;
+  readonly title: string;
+}
+
+export function get_channel_details(
   api_key: string,
   raw_handle: string,
-): Promise<string | undefined>;
+): Promise<ChannelDetails | undefined>;
